83e709922d73c0dcf4e664339444a93461aa964a
heiwa4126 <heiwa4126@gmail.com>
2022-11-15 10:31:26 +0900

    cleanup

--- main.go
+++ main.go
@@ -11,16 +11,6 @@ func now() string {
 
 func main() {
 	document := js.Global().Get("document")
-	// document = window.document;
-
-	myH1 := document.Call("createElement", "h1")
-	// myH1 = document.createElement("h1");
-
-	myH1.Set("innerText", "This is H1")
-	// myH1.innerHTML = "This is H1";
-
-	document.Get("body").Call("appendChild", myH1)
-	// document.body.appendChild(myH1);
 
 	timestamp := document.Call("getElementById", "now")
 	timestamp.Set("innerText", now())
